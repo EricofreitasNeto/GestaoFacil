b4262336a8db180f2b728f1dd69b3cd34924ef51
EricofreitasNeto <ericofreitas123@hotmail.com>
2025-08-31 21:31:09 -0300

    Commit config vercel p3

--- api/index.js
+++ api/index.js
@@ -1,14 +1,5 @@
 const app = require('./app');
-require('dotenv').config();
 
-if (process.env.APP_MODE === 'local') {
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => {
-    console.log(`🟢 Rodando localmente em http://localhost:${PORT}`);
-  });
-} else {
-  console.log('🚀 Rodando em modo serverless (Vercel)');
-}
 module.exports = (req, res) => {
   app(req, res);
 };
