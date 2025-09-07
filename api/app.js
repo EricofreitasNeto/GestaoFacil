bf9b982573de25e63f290fab892c8c18be2892df
EricofreitasNeto <ericofreitas123@hotmail.com>
2025-09-07 01:01:11 -0300

    Teste13

--- api/app.js
+++ api/app.js
@@ -162,7 +162,7 @@ function getLocalIP() {
   }
   return 'localhost';
 }
-
+//khsdkjhkjhdkjhskjhdkjsh
 async function startServer() {
   console.log(`🧠 APP_MODE: ${APP_MODE}, USE_HTTPS: ${USE_HTTPS}`);
   console.log("DATABASE_URL:", process.env.DATABASE_URL);
